b65ef3c4ccdf81c8b4a16ae0f5e8094931549615
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2025-05-31 17:32:56 +0700

    Exercise 4: Perspective decoration

---
+++ src/components/PerspectiveDecoration/PerspectiveDecoration.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PerspectiveDecoration.module.css';
+
+const PerspectiveDecoration = () => {
+  return <div className={styles.wrapper}></div>;
+};
+
+export default PerspectiveDecoration;
