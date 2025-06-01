0fb3f2fbe74694ff6ebed82fd92988d699a6088c
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2025-06-01 14:53:03 +0700

    Check out referred solution

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -26,7 +26,11 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
-      <PerspectiveDecoration />
+      {/* my solution */}
+      {/* <PerspectiveDecoration /> */}
+
+      {/* Referred solution */}
+      <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
